54c687536ecbed21242454cade851ca5f80606be
Christophe Paleyron <67900397+CryzerFranz@users.noreply.github.com>
2024-02-18 00:20:04 +0100

    adjust some props for Cannabis

--- Contents/mods/Cryzers-Drugs/media/lua/server/CDrugsconf.lua
+++ Contents/mods/Cryzers-Drugs/media/lua/server/CDrugsconf.lua
@@ -22,7 +22,7 @@ CDrugsconf.growNewPlant = function(planting, nextGrowing, updateNbofGrow)
 			planting.nextGrowing = calcNextGrowing(nextGrowing, farming_vegetableconf.props[planting.typeOfSeed].timeToGrow + water + diseaseLvl);
 			planting:setObjectName(farming_vegetableconf.getObjectName(planting));
 			planting:setSpriteName(farming_vegetableconf.getSpriteName(planting));
-			planting.hasVegetable = true;
+			-- planting.hasVegetable = true;
 		else
 			badPlant(water, nil, diseaseLvl, planting, nextGrowing, updateNbofGrow);
 		end
@@ -47,7 +47,7 @@ end
 -- PLANT PROPERTIES --
 
 -- Icons
-farming_vegetableconf.icons["Cannabis"] = "Item_CDCannabisPlant";
+farming_vegetableconf.icons["Cannabis"] = "cd_farming_cnbs_7";
 
 -- Props & Sprites
 
@@ -55,13 +55,13 @@ farming_vegetableconf.icons["Cannabis"] = "Item_CDCannabisPlant";
 farming_vegetableconf.props["Cannabis"] = {};
 
 farming_vegetableconf.props["Cannabis"].seedsRequired = 4;
-farming_vegetableconf.props["Cannabis"].texture = "cd_farming_cnbs_6";
+farming_vegetableconf.props["Cannabis"].texture = "cd_farming_cnbs_7";
 farming_vegetableconf.props["Cannabis"].waterLvl = 80;
 farming_vegetableconf.props["Cannabis"].timeToGrow = ZombRand(89, 103)
-farming_vegetableconf.props["Cannabis"].minVeg = 3;
-farming_vegetableconf.props["Cannabis"].maxVeg = 6;
-farming_vegetableconf.props["Cannabis"].minVegAutorized = 10;
-farming_vegetableconf.props["Cannabis"].maxVegAutorized = 15;
+farming_vegetableconf.props["Cannabis"].minVeg = 1;
+farming_vegetableconf.props["Cannabis"].maxVeg = 1;
+farming_vegetableconf.props["Cannabis"].minVegAutorized = 1;
+farming_vegetableconf.props["Cannabis"].maxVegAutorized = 1;
 farming_vegetableconf.props["Cannabis"].vegetableName = "Cryzers_Drugs.Cannabis";
 farming_vegetableconf.props["Cannabis"].seedName = "Cryzers_Drugs.CannabisSeed";
 farming_vegetableconf.props["Cannabis"].seedPerVeg = 3;
@@ -75,7 +75,6 @@ farming_vegetableconf.sprite["Cannabis"] = {
 "cd_farming_cnbs_4",
 "cd_farming_cnbs_5",
 "cd_farming_cnbs_6",
-"cd_farming_cnbs_7",
-"cd_farming_cnbs_8",
-
+"cd_farming_cnbs_7", -- mature: hasVegetable = true & hasSeed = true
+"cd_farming_cnbs_8", -- rotten
 }
